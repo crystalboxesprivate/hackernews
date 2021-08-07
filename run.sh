b9d32b26c4f3ec5e1f932a1c7d9f843bf03c9b26
Oleg Pustovit <crystalboxesgfx@gmail.com>
2021-08-07 18:15:11 +0300

    Add multiple options for running the script

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-echo "(load \"news.arc\")(nsv)" | mzscheme -f /app/src/as.scm
+if [ "${APP}" == "blog" ]; then
+  echo "(load \"blog.arc\")(bsv)" | mzscheme -f /app/src/as.scm
+elif [ "${APP}" == "news" ]; then
+  echo "(load \"news.arc\")(nsv)" | mzscheme -f /app/src/as.scm
+else
+  mzscheme -f /app/src/as.scm
+fi
