10fd28972c1c4ce460a06993f8e254e0549cbefb
Oleg Pustovit <crystalboxesgfx@gmail.com>
2021-08-07 18:20:55 +0300

    Fix paths

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 if [ "${APP}" == "blog" ]; then
   echo "(load \"blog.arc\")(bsv)" | mzscheme -f /app/src/as.scm
+  exit
 elif [ "${APP}" == "news" ]; then
   echo "(load \"news.arc\")(nsv)" | mzscheme -f /app/src/as.scm
+  exit
 else
-  mzscheme -f /app/src/as.scm
+  mzscheme -f as.scm
 fi
